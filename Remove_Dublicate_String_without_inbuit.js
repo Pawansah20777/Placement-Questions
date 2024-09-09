ed4dce8bfd1f1461f37c2b272efbe0d52010225f
pawan kumar <pawansah20777@gmail.com>
2024-09-10 00:36:34 +0530

    added

--- Remove_Dublicate_String_without_inbuit.js
+++ Remove_Dublicate_String_without_inbuit.js
@@ -1,15 +1,15 @@
-function abc(str) {
-    let a = '';
-    for (let i = 0; i < str.length; i++) {
+function abc(arr) {
+    let a = []
+    for (let i = 0; i < arr.length; i++) {
 
-      if (a.indexOf(str[i]) === -1) {
-           a=a+str[i];
+      if (a.indexOf(arr[i]) === -1) {
+           a.push(arr[i])
       }
 
     }
     return a;
   }
   
-  const result = abc("hello");
+  const result = abc([1,2,2,3,4]);
   console.log(result);
   
