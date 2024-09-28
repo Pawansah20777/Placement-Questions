c13445a4cfde17cbd13503968b96c44e31a44c35
pawan kumar <pawansah20777@gmail.com>
2024-09-28 17:49:04 +0530

    add

---
+++ Single_row_statement.js
@@ -0,0 +1,8 @@
+for(var i = 1; i <=10; i++){
+  process.stdout.write(i + ' '); 
+}
+// How it works:
+// i is a number (integer).
+// ' ' is a string (a single space character).
+// When you use the + operator between a number and a string, 
+//JavaScript converts the number into a string and then concatenates them.
