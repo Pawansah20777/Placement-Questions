ba5c4421eb07a5526f75d28db6db8b9d55fcae37
pawan kumar <pawansah20777@gmail.com>
2024-09-28 17:34:43 +0530

    add

---
+++ Swap.js
@@ -0,0 +1,20 @@
+//swap with third varibale
+// let a=2;
+// let b=3;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a,b);
+
+//swap without third varibale
+
+// let a = 2;
+// let b = 3;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(a,b);
