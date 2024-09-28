aa009fda3c0ff1a15266021c0ab82cba32467d16
pawan kumar <pawansah20777@gmail.com>
2024-09-28 18:37:38 +0530

    addd

---
+++ Join.js
@@ -0,0 +1,30 @@
+//join(): Convert an array into a string with a specified separator
+//If you remove the separator from join(), it defaults to using a comma (,) as the separator.
+
+// const fruits = ['apple', 'banana', 'orange'];
+// const result = fruits.join(",");  
+// console.log(result); 
+
+//=========================================================
+
+// const fruits = ['apple', 'banana', 'orange'];
+// const result = fruits.join();  
+// console.log(result); 
+
+//=========================================================
+
+// const words = ['hello', 'world', 'javascript'];
+// const result = words.join("-");  
+// console.log(result);  
+
+//=========================================================
+
+// const numbers = [1, 2, 3, 4];
+// const result = numbers.join('');  // No separator
+// console.log(result);  
+
+//=========================================================
+
+// const items = ['car', 'bike', 'boat'];
+// const result = items.join(' | ');  // Custom separator
+// console.log(result);  
---
+++ Split.js
@@ -0,0 +1,43 @@
+//split(): Convert a string into an array using a specified separator
+//If you do not pass a separator to the split() method in JavaScript, 
+//it will treat the entire string as a single element and return an array containing that string.
+
+// const str = 'apple,banana,orange';
+// const result = str.split(',');  // Split by comma
+// console.log(result);  
+
+// const str = 'apple,banana,orange';
+// const result = str.split('');   // Split every character
+// console.log(result);  
+
+// const str = 'apple,banana,orange';
+// const result = str.split();   
+// console.log(result);  
+
+// const str = 'apple,banana,orange';
+// const result = str.split(' '); 
+// console.log(result);  
+
+// const sentence = 'Hello world JavaScript';
+// const result = sentence.split(' ');  // Split by space
+// console.log(result);  
+
+// const sentence = 'Hello world JavaScript';
+// const result = sentence.split();  
+// console.log(result); 
+
+// const data = '2024-09-28';
+// const result = data.split('-');  // Split by hyphen
+// console.log(result);  
+
+// const text = 'hello';
+// const result = text.split('');  // Split every character
+// console.log(result); 
+
+// const path = 'folder/subfolder/file.txt';
+// const result = path.split('/');  // Split by slash
+// console.log(result);  
+
+// const multiSpaceString = 'This is    a test';  // Multiple spaces
+// const result = multiSpaceString.split(' ');  
+// console.log(result);  
