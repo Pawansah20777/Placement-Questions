b08accf98bcfafca80f60530876a275a836d9dda
pawan kumar <pawansah20777@gmail.com>
2024-10-01 15:15:02 +0530

    add

---
+++ Max_Min.js
@@ -0,0 +1,9 @@
+//Find Max Number
+let a = [1, 2, 3, 4, 5];
+let Max = a.reduce((prev, curr) =>prev > curr ? prev : curr);
+console.log(Max);
+
+//Find Min Number
+let a1 = [1, 2, 3, 4, 5];
+let Min = a.reduce((prev, curr) =>prev < curr ? prev : curr);
+console.log(Min);
