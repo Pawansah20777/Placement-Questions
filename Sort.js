b48e76511b66b84cfbacf89c690a63009cd20718
pawan kumar <pawansah20777@gmail.com>
2025-06-08 09:11:52 +0530

    add

--- Sort.js
+++ Sort.js
@@ -4,6 +4,6 @@ console.log(b); // Outputs: [1, 2, 6, 14, 53]
 console.log(a); // Outputs: [1, 2, 6, 14, 53] (original array is modified)
 
 let c = [2, 6, 14, 53, 1];
-let d = [...c].sort((x, y) => x - y); // Create a copy of a before sorting
+let d = [...c].sort((x, y) => x - y); // Create a copy of c before sorting
 console.log(d); // Outputs: [1, 2, 6, 14, 53]
 console.log(c); // Outputs: [2, 6, 14, 53, 1] (original array remains unchanged)
